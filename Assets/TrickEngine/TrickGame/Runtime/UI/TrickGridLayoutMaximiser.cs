46ec56311c14a4c256686644b318dfd0e5cf3bb6
Tuan <tuan1992_10@hotmail.com>
2023-08-18 21:55:19 +0200

    Refactor TrickGridLayoutMaximiser.cs to improve grid size maximization.

--- Assets/TrickEngine/TrickGame/Runtime/UI/TrickGridLayoutMaximiser.cs
+++ Assets/TrickEngine/TrickGame/Runtime/UI/TrickGridLayoutMaximiser.cs
@@ -6,88 +6,92 @@ namespace TrickCore
 {
     /// <summary>
     /// GridLayoutGroup helper function to maximize the grid size
-    /// https://forum.unity.com/threads/flexible-grid-layout.296074/ by @Arkade
     /// </summary>
     [RequireComponent(typeof(GridLayoutGroup))]
     [ExecuteAlways]
     public sealed class TrickGridLayoutMaximiser : MonoBehaviour
     {
-
-        [Tooltip("Override the number of columns to aim for (or zero for default/disabled).\n" +
+        [Tooltip("Override the number of columns to aim for (or zero for default/disabled). " +
                  "Takes priority over rows.")]
         [SerializeField]
         private int numColumnsOverride = 0;
 
-        [Tooltip("Override the number of rows to aim for (or zero for default/disabled).")] [SerializeField]
+        [Tooltip("Override the number of rows to aim for (or zero for default/disabled).")]
+        [SerializeField]
         private int numRowsOverride = 0;
 
-        public void OnEnable()
+        private bool isSettingSizes = false; // A flag to prevent recursive invocation
+
+        private void OnEnable()
         {
-            setSizes();
+            SetSizes();
         }
 
         private void OnRectTransformDimensionsChange()
         {
-            setSizes();
+            SetSizes();
         }
 
         [ContextMenu("Set sizes")]
-        private void setSizes()
+        private void SetSizes()
         {
+            // Check for recursion
+            if (isSettingSizes)
+            {
+                return;
+            }
+            isSettingSizes = true;
+
             var gridLayoutGroup = GetComponent<GridLayoutGroup>();
-            var columns = this.numColumnsOverride;
-            var rows = this.numRowsOverride;
+
+            int columns = this.numColumnsOverride;
+            int rows = this.numRowsOverride;
+
             switch (gridLayoutGroup.constraint)
             {
-                case GridLayoutGroup.Constraint.Flexible: // nop
+                case GridLayoutGroup.Constraint.Flexible:
+                    // Do nothing
                     break;
-
                 case GridLayoutGroup.Constraint.FixedColumnCount:
                     columns = gridLayoutGroup.constraintCount;
                     break;
-
                 case GridLayoutGroup.Constraint.FixedRowCount:
                     rows = gridLayoutGroup.constraintCount;
                     break;
-
                 default:
                     throw new ArgumentOutOfRangeException(gridLayoutGroup.constraint.ToString());
             }
 
             var padding = gridLayoutGroup.padding;
             var spacing = gridLayoutGroup.spacing;
+
             var size = ((RectTransform)transform).rect.size - new Vector2(padding.horizontal, padding.vertical);
-            float width, height;
-            if (0 < columns)
+
+            float width = 0, height = 0;
+
+            if (columns > 0)
             {
                 width = (size.x - (columns - 1) * spacing.x) / columns;
-                if (0 < rows)
-                {
-                    height = (size.y - (rows - 1) * spacing.y) / rows;
-                }
-                else
-                {
-                    // TODO: account for different vertical to horizontal spacing
-                    height = width;
-                }
             }
-            else
+
+            if (rows > 0)
             {
-                if (0 < rows)
-                {
-                    // rows specified but not columns
-                    // TODO: account for different vertical to horizontal spacing
-                    width = height = (size.y - (rows - 1) * spacing.y) / rows;
-                }
-                else
-                {
-                    // neither specified
-                    return;
-                }
+                height = (size.y - (rows - 1) * spacing.y) / rows;
+            }
+
+            // If rows or columns not defined, set height or width respectively
+            if (columns <= 0)
+            {
+                width = height;
+            }
+            else if (rows <= 0)
+            {
+                height = width;
             }
 
             gridLayoutGroup.cellSize = new Vector2(width, height);
-        }
 
+            isSettingSizes = false;
+        }
     }
 }
