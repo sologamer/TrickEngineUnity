c2640d917623d3655d12725adbdd3b2ca40edb6a
Tuan <tuan1992_10@hotmail.com>
2024-02-17 07:38:45 +0100

    LoadingMenu self managed mode

--- Assets/TrickEngine/TrickGame/Runtime/UI/LoadingMenu.cs
+++ Assets/TrickEngine/TrickGame/Runtime/UI/LoadingMenu.cs
@@ -9,6 +9,7 @@ namespace TrickCore
 {
     public class LoadingMenu : UIMenu
     {
+        public bool SelfManaged = true;
         public string DefaultWaitText = "LOADING...";
         public TextMeshProUGUI WaitText;
         public Image FillProgress;
@@ -22,9 +23,17 @@ namespace TrickCore
             if (ProgressText != null) ProgressText.text = $"{progress * 100.0f:F0}%";
         }
         
-        public virtual LoadingMenu WaitFor(string waitText, Action onLoadAction, Func<float> waitCondition, float waitCompleteDelay = 0.25f, float waitConditionInterval = 0.1f)
+        public IEnumerator WaitForRoutine(string waitText, Action<LoadingMenu> onLoadAction, Func<float> waitCondition, float waitCompleteDelay = 0.25f, float waitConditionInterval = 0.1f)
+        {
+            yield return WaitFor(waitText, onLoadAction, waitCondition, waitCompleteDelay, waitConditionInterval);
+        }
+
+        public virtual LoadingMenu WaitFor(string waitText, Action<LoadingMenu> onLoadAction, Func<float> waitCondition, float waitCompleteDelay = 0.25f, float waitConditionInterval = 0.1f)
         {
             if (WaitText != null) WaitText.text = string.IsNullOrEmpty(waitText) ? DefaultWaitText : waitText;
+            Routine.Replace(CustomWaiter());
+
+            return this;
 
             IEnumerator CustomWaiter()
             {
@@ -35,27 +44,23 @@ namespace TrickCore
                     return progress >= 1.0f;
                 }, waitConditionInterval);
                 if (waitCompleteDelay > 0) yield return Routine.WaitSeconds(waitCompleteDelay);
-                Hide();
-                onLoadAction?.Invoke();
+                if (!SelfManaged) Hide();
+                onLoadAction?.Invoke(this);
             }
-        
-            Routine.Replace(CustomWaiter());
-
-            return this;
         }
     
-        public virtual LoadingMenu WaitForSceneLoad(string waitText, int buildIndex, Action onLoadAction)
+        public virtual LoadingMenu WaitForSceneLoad(string waitText, int buildIndex, Action<LoadingMenu> onLoadAction)
         {
             WaitText.text = string.IsNullOrEmpty(waitText) ? DefaultWaitText : waitText;
-        
+            Routine.Replace(Waiter());
+            return this;
+
             IEnumerator Waiter()
             {
                 yield return Routine.WaitCondition(() => SceneManager.GetActiveScene().buildIndex == buildIndex, 0.5f);
-                Hide();
-                onLoadAction?.Invoke();
+                if (!SelfManaged) Hide();
+                onLoadAction?.Invoke(this);
             }
-            Routine.Replace(Waiter());
-            return this;
         }
     }
 }
